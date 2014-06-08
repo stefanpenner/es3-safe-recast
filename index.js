8bf0c33e451822ed957dabede83e85f57a8ef98c
Devin Torres <devin@devintorr.es>
2014-06-08 17:15:25 -0500

    Recast all ECMAScript 3 reserved words

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var recast = require('recast');
 
 var Visitor = recast.Visitor;
@@ -5,14 +7,74 @@ var types = recast.types;
 var namedTypes = types.namedTypes;
 var builders = types.builders;
 
+// http://www.ecma-international.org/publications/files/ECMA-ST-ARCH/ECMA-262,%203rd%20edition,%20December%201999.pdf
 var identifierToLiteral = {
-  finally: true,
-  catch:   true,
+  // Keyword
+  break: true,
+  case: true,
+  catch: true,
+  continue: true,
   default: true,
-  new:     true,
-  throw:   true,
-  return:  true,
-  import:  true
+  delete: true,
+  do: true,
+  else: true,
+  finally: true,
+  for: true,
+  function: true,
+  if: true,
+  in: true,
+  instanceof: true,
+  new: true,
+  return: true,
+  switch: true,
+  this: true,
+  throw: true,
+  try: true,
+  typeof: true,
+  var: true,
+  void: true,
+  while: true,
+  with: true,
+
+  // FutureReservedWords
+  abstract: true,
+  boolean: true,
+  byte: true,
+  char: true,
+  class: true,
+  const: true,
+  debugger: true,
+  double: true,
+  enum: true,
+  export: true,
+  extends: true,
+  final: true,
+  float: true,
+  goto: true,
+  implements: true,
+  import: true,
+  int: true,
+  interface: true,
+  long: true,
+  native: true,
+  package: true,
+  private: true,
+  protected: true,
+  public: true,
+  short: true,
+  static: true,
+  super: true,
+  synchronized: true,
+  throws: true,
+  transient: true,
+  volatile: true,
+
+  // NullLiteral
+  null: true,
+
+  // BooleanLiteral
+  true: true,
+  false: true
 };
 
 var ES6Safe = Visitor.extend({
